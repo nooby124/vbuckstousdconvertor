3b9f85d9852a4b99acd0b8319d9849121055917d
nooby124 <amitay33126@gmail.com>
2023-11-06 21:22:15 +0200

    initial init or something idk

---
+++ index.js
@@ -0,0 +1,25 @@
+function convert() {
+    document.getElementById("theamountinvbucks").innerHTML = ("The amount in USD: " + 0.00799 * Number(document.getElementById("numberofmoney").value))
+}
+
+function setInputFilter(textbox, inputFilter) {
+    ["input", "keydown", "keyup", "mousedown", "mouseup", "select", "contextmenu", "drop"].forEach(function(event) {
+      textbox.addEventListener(event, function() {
+        if (inputFilter(this.value)) {
+          this.oldValue = this.value;
+          this.oldSelectionStart = this.selectionStart;
+          this.oldSelectionEnd = this.selectionEnd;
+        } else if (this.hasOwnProperty("oldValue")) {
+          this.value = this.oldValue;
+          this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+        } else {
+          this.value = "";
+        }
+      });
+    });
+  }
+  
+setInputFilter(document.getElementById("numberofmoney"), function(value) {
+    return /^\d*$/.test(value);
+  });
+document.getElementById("submit").addEventListener("click", convert, false);
